23573bb093be51233e6456939a16d782140a3dda
Agustt123 <agustintracheskyoficial@gmail.com>
2025-04-16 10:22:44 -0300

    aaaa

--- api-colecta.js
+++ api-colecta.js
@@ -2,12 +2,14 @@ import express, { json, urlencoded } from 'express';
 import colecta from './routes/colecta.js';
 import { redisClient } from './db.js';
 import { logPurple } from './src/funciones/logsCustom.js';
+import cors from 'cors';
 
 const app = express();
 
 app.use(json({ limit: '50mb' }));
 app.use(urlencoded({ limit: '50mb', extended: true }));
 app.use(json());
+app.use(cors());
 
 const PORT = process.env.PORT || 13000;
 
@@ -19,7 +21,21 @@ app.post('/api/testapi', async (req, res) => {
     res.status(200).json({ message: 'API funcionando correctamente' });
 });
 
-
+app.get('/ping', (req, res) => {
+    const currentDate = new Date();
+    currentDate.setHours(currentDate.getHours()); // Resta 3 horas
+  
+    // Formatear la hora en el formato HH:MM:SS
+    const hours = currentDate.getHours().toString().padStart(2, '0');
+    const minutes = currentDate.getMinutes().toString().padStart(2, '0');
+    const seconds = currentDate.getSeconds().toString().padStart(2, '0');
+  
+    const formattedTime = `${hours}:${minutes}:${seconds}`;
+  
+    res.status(200).json({
+      hora: formattedTime
+    });
+  });
 await redisClient.connect();
 
 app.listen(PORT, () => {
--- routes/colecta.js
+++ routes/colecta.js
@@ -7,6 +7,7 @@ import mysql from "mysql";
 import { crearLog } from '../src/funciones/crear_log.js';
 
 
+
 const colecta = Router();
 const dbConfigLocal = getLocalDbConfig();
 const dbConnectionLocal = mysql.createConnection(dbConfigLocal);
@@ -21,20 +22,28 @@ colecta.post('/colecta', async (req, res) => {
         logPurple(`Tiempo de ejecución: ${endTime - startTime} ms`);
         return res.status(400).json({ message: errorMessage });
     }
-
+const body = req.body;
     const { companyId, userId, profile, dataQr, autoAssign } = req.body;
 
 
+    const company = await getCompanyById(companyId);
+    
+    console.log("hoila");
+    
     try {
-        const company = await getCompanyById(companyId);
-
-
-
+        const endTime = performance.now();
+        const tiempo = endTime - startTime;
         const result = await colectar(company, dataQr, userId, profile, autoAssign,dbConnectionLocal);
-crearLog(companyId,userId,dataQr.did || 0, "1", req.body,userId,dbConnectionLocal,JSON.stringify(result));
+        crearLog(dbConnectionLocal, company.did, userId, body.profile, body, tiempo, JSON.stringify(result), "api", true);
+
         res.status(200).json(result);
     } catch (error) {
-        crearLog(companyId,userId,dataQr.did || 0, "-1", req.body,userId,dbConnectionLocal,error.message);
+        const endTime = performance.now();
+        const tiempo = endTime - startTime;
+    
+      
+        
+        crearLog(dbConnectionLocal, company.did, userId, body.profile, body, tiempo, JSON.stringify(error), "api", false);
 
         res.status(500).json({ message: error.message });
     } finally {
