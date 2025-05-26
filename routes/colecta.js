f039eeb0977521da67f8b14278c618a7d7ae4d3d
Agustt123 <agustintracheskyoficial@gmail.com>
2025-05-26 10:54:32 -0300

    cambios piola

--- routes/colecta.js
+++ routes/colecta.js
@@ -1,48 +1,76 @@
-import { Router } from 'express';
-import { colectar } from '../controller/colectaController.js';
-import { getCompanyById, getLocalDbConfig } from '../db.js';
-import { verifyParameters } from '../src/funciones/verifyParameters.js';
-import { logPurple } from '../src/funciones/logsCustom.js';
+import { Router } from "express";
+import { colectar } from "../controller/colectaController.js";
+import { getCompanyById, getLocalDbConfig } from "../db.js";
+import { verifyParameters } from "../src/funciones/verifyParameters.js";
+import { logPurple } from "../src/funciones/logsCustom.js";
 import mysql from "mysql";
-import { crearLog } from '../src/funciones/crear_log.js';
-
-
+import { crearLog } from "../src/funciones/crear_log.js";
 
 const colecta = Router();
 const dbConfigLocal = getLocalDbConfig();
 const dbConnectionLocal = mysql.createConnection(dbConfigLocal);
 dbConnectionLocal.connect();
 
-colecta.post('/colecta', async (req, res) => {
-    const startTime = performance.now();
-    const errorMessage = verifyParameters(req.body, ['dataQr', 'autoAssign', 'deviceFrom']);
+colecta.post("/colecta", async (req, res) => {
+  const startTime = performance.now();
+  const errorMessage = verifyParameters(req.body, [
+    "dataQr",
+    "autoAssign",
+    "deviceFrom",
+  ]);
 
-    if (errorMessage) {
-        const endTime = performance.now();
-        logPurple(`Tiempo de ejecución: ${endTime - startTime} ms`);
-        return res.status(400).json({ message: errorMessage });
-    }
-const body = req.body;
-    const { companyId, userId, profile, dataQr, autoAssign } = req.body;
+  if (errorMessage) {
+    const endTime = performance.now();
+    logPurple(`Tiempo de ejecución: ${endTime - startTime} ms`);
+    return res.status(400).json({ message: errorMessage });
+  }
 
+  const body = req.body;
+  const { companyId, userId, profile, dataQr, autoAssign } = req.body;
 
-    try {
-        const company = await getCompanyById(companyId);
+  try {
+    const company = await getCompanyById(companyId);
 
+    const result = await colectar(
+      company,
+      dataQr,
+      userId,
+      profile,
+      autoAssign,
+      dbConnectionLocal
+    );
+    const endTime = performance.now();
+    crearLog(
+      dbConnectionLocal,
+      companyId,
+      userId,
+      profile || 0,
+      body,
+      endTime - startTime,
+      result,
+      "api",
+      true
+    );
+    res.status(200).json(result);
+  } catch (error) {
+    const endTime = performance.now();
+    crearLog(
+      dbConnectionLocal,
+      companyId,
+      userId,
+      profile || 0,
+      body,
+      endTime - startTime,
+      error.stack || error.message,
+      "api",
+      false
+    );
+    res.status(500).json({ message: error.message });
+  } finally {
+    const endTime = performance.now();
 
-
-        const result = await colectar(company, dataQr, userId, profile, autoAssign,dbConnectionLocal);
-crearLog(companyId,userId,dataQr.did || 0, "1", req.body,userId,dbConnectionLocal,JSON.stringify(result));
-        res.status(200).json(result);
-    } catch (error) {
-        crearLog(companyId,userId,dataQr.did || 0, "-1", req.body,userId,dbConnectionLocal,error.message);
-
-        res.status(500).json({ message: error.message });
-    } finally {
-        const endTime = performance.now();
-
-        logPurple(`Tiempo de ejecución: ${endTime - startTime} ms`);
-    }
+    logPurple(`Tiempo de ejecución: ${endTime - startTime} ms`);
+  }
 });
 
 export default colecta;
--- src/funciones/crear_log.js
+++ src/funciones/crear_log.js
@@ -1,16 +1,35 @@
 import { executeQuery } from "../../db.js";
 import { logGreen, logRed } from "./logsCustom.js";
 
-export async function crearLog(dbConnection, empresa, usuario, perfil, body, tiempo, resultado, metodo, exito) {
-    try {
-        const sqlLog = `INSERT INTO logs_v2 (empresa, usuario, perfil, body, tiempo, resultado, metodo, exito) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+export async function crearLog(
+  dbConnection,
+  empresa,
+  usuario,
+  perfil,
+  body,
+  tiempo,
+  resultado,
+  metodo,
+  exito
+) {
+  try {
+    const sqlLog = `INSERT INTO logs_v2 (empresa, usuario, perfil, body, tiempo, resultado, metodo, exito) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
 
-        const values = [empresa, usuario, perfil, JSON.stringify(body), tiempo, JSON.stringify(resultado), metodo, exito];
+    const values = [
+      empresa,
+      usuario,
+      perfil,
+      JSON.stringify(body),
+      tiempo,
+      JSON.stringify(resultado),
+      metodo,
+      exito,
+    ];
 
-        await executeQuery(dbConnection, sqlLog, values, true);
-        logGreen(`Log creado: ${JSON.stringify(values)}`);
-    } catch (error) {
-        logRed(`Error en crearLog: ${error.stack}`)
-        throw error;
-    }
+    await executeQuery(dbConnection, sqlLog, values, true);
+    logGreen(`Log creado: ${JSON.stringify(values)}`);
+  } catch (error) {
+    logRed(`Error en crearLog: ${error.stack}`);
+    throw error;
+  }
 }
