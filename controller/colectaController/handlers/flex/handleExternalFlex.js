1f29400bc8e154e4bc0aa1b785068eb37e9b1da1
Agustt123 <agustintracheskyoficial@gmail.com>
2025-06-05 14:35:41 -0300

    ca

--- controller/colectaController/handlers/flex/handleExternalFlex.js
+++ controller/colectaController/handlers/flex/handleExternalFlex.js
@@ -81,10 +81,12 @@ export async function handleExternalFlex(
                         WHERE ml_shipment_id = ? AND ml_vendedor_id = ? 
                         LIMIT 1
                     `;
-      let rowsEnvios = await executeQuery(externalDbConnection, sqlEnvios, [
-        shipmentId,
-        senderid,
-      ]);
+      let rowsEnvios = await executeQuery(
+        externalDbConnection,
+        sqlEnvios,
+        [shipmentId, senderid],
+        true
+      );
 
       let externalShipmentId;
       let externalClientId;
