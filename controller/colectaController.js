4a6435426fd583cf7a0755f0457c829a4adff4a5
Agustt123 <agustintracheskyoficial@gmail.com>
2025-07-10 15:31:47 -0300

    CAMBIOS 144 EMPRESA

--- controller/colectaController.js
+++ controller/colectaController.js
@@ -1,4 +1,4 @@
-import { getAccountBySenderId, getProdDbConfig } from "../db.js";
+import { executeQuery, getAccountBySenderId, getProdDbConfig } from "../db.js";
 import { handleInternalFlex } from "./colectaController/handlers/flex/handleInternalFlex.js";
 import { handleExternalFlex } from "./colectaController/handlers/flex/handleExternalFlex.js";
 import { handleExternalNoFlex } from "./colectaController/handlers/noflex/handleExternalNoFlex.js";
@@ -7,6 +7,8 @@ import mysql from "mysql";
 import { logCyan, logRed } from "../src/funciones/logsCustom.js";
 import { parseIfJson } from "../src/funciones/isValidJson.js";
 import axios from "axios";
+
+
 async function getShipmentIdFromQr(companyId, dataQr) {
     const payload = {
         companyId: Number(companyId),
@@ -22,7 +24,7 @@ async function getShipmentIdFromQr(companyId, dataQr) {
     };
 
     const result = await axios.post('https://apimovil2.lightdata.app/api/qr/get-shipment-id', payload);
-    if (result.status == 200) {
+    if (result.status === 200) {
         return result.data.body;
     } else {
         logRed("Error al obtener el shipmentId");
@@ -31,15 +33,19 @@ async function getShipmentIdFromQr(companyId, dataQr) {
 }
 
 export async function colectar(company, dataQr, userId, profile, autoAssign, latitude, longitude) {
-
     const dbConfig = getProdDbConfig(company);
     const dbConnection = mysql.createConnection(dbConfig);
     dbConnection.connect();
+
     try {
         let response;
-
         dataQr = parseIfJson(dataQr);
-        if (company.did == 211 && !Object.prototype.hasOwnProperty.call(dataQr, "local") && !Object.prototype.hasOwnProperty.call(dataQr, "sender_id")) {
+
+        if (
+            company.did === 211 &&
+            !Object.prototype.hasOwnProperty.call(dataQr, "local") &&
+            !Object.prototype.hasOwnProperty.call(dataQr, "sender_id")
+        ) {
             const shipmentId = await getShipmentIdFromQr(company.did, dataQr);
             dataQr = {
                 local: "1",
@@ -48,48 +54,64 @@ export async function colectar(company, dataQr, userId, profile, autoAssign, lat
                 cliente: 301
             };
         }
-        /// Me fijo si es flex o no
+
         const isFlex = Object.prototype.hasOwnProperty.call(dataQr, "sender_id");
 
-        /// Si es flex
         if (isFlex) {
             logCyan("Es flex");
-            /// Busco la cuenta del cliente
+
             const account = await getAccountBySenderId(dbConnection, company.did, dataQr.sender_id);
 
-            /// Si la cuenta existe, es interno
             if (account) {
                 logCyan("Es interno");
                 response = await handleInternalFlex(dbConnection, company.did, userId, profile, dataQr, autoAssign, account, latitude, longitude);
+            }
 
-                /// Si la cuenta no existe, es externo
+
+            if (!account && company.did == 144) {
+                console.log("Empresa 144, chequeo extra de empresa");
+
+                const queryCheck = `
+                    SELECT did
+                    FROM envios
+                    WHERE ml_vendedor_id = ?
+                        AND superado = 0
+                        AND elim = 0
+                    LIMIT 1
+                `;
+                const resultCheck = await executeQuery(dbConnection, queryCheck, [dataQr.sender_id], true);
+
+                if (resultCheck.length > 0) {
+                    logCyan("Es interno (por verificación extra de empresa 144)");
+                    response = await handleInternalFlex(dbConnection, company.did, userId, profile, dataQr, autoAssign, account, latitude, longitude);
+                } else {
+                    logCyan("Es externo");
+                    response = await handleExternalFlex(dbConnection, company, userId, profile, dataQr, autoAssign, latitude, longitude);
+                }
             } else {
                 logCyan("Es externo");
                 response = await handleExternalFlex(dbConnection, company, userId, profile, dataQr, autoAssign, latitude, longitude);
             }
-            /// Si no es flex
+
         } else {
             logCyan("No es flex");
-            /// Si la empresa del QR es la misma que la empresa del usuario, es interno
+
             if (company.did == dataQr.empresa) {
                 logCyan("Es interno");
                 response = await handleInternalNoFlex(dbConnection, dataQr, company.did, userId, profile, autoAssign, latitude, longitude);
-
-                /// Si la empresa del QR es distinta a la empresa del usuario, es externo
             } else {
                 logCyan("Es externo");
                 response = await handleExternalNoFlex(dbConnection, dataQr, company.did, userId, profile, autoAssign, latitude, longitude);
             }
         }
 
-
         return response;
-    } catch (error) {
-        logRed("Error en colectar: ", error.message);
-        throw error;
 
+    } catch (error) {
+        logRed("Error en colectar:", error.message);
+        throw error;
 
     } finally {
         dbConnection.end();
-    };
+    }
 }
