8d2329e4cf310f97398f92969392d62d119d307f
denisegonzalez24 <nissgonzalezm@gmail.com>
2025-07-30 10:08:10 -0300

    refactor: consolidate tieneBarcode structure and unify getSenderId method in LogisticaConf class

--- classes/logisticas_conf.js
+++ classes/logisticas_conf.js
@@ -1,17 +1,6 @@
 export default class LogisticaConf {
 
-
-
-    // el array debe tener la vnculacion entre logisticas ademas del did_vendedor asociado 
     static tieneBarcode = {
-        12: 44,
-        55: 184,
-        211: 301,
-        20: 215,
-        327: 15,
-    };
-
-    static tieneBarcode2 = {
         20: { empresa_vinculada: 211, seller_id: 215 },
         211: { empresa_vinculada: null, seller_id: 301 },
         12: { empresa_vinculada: 55, seller_id: 44 },
@@ -27,21 +16,11 @@ export default class LogisticaConf {
         return String(did) in this.tieneBarcode;
     }
 
-    static getSenderId(did) {
-        return this.tieneBarcode?.[String(did)] ?? 0;
-    }
-
     static getEmpresaVinculada(did) {
-        return this.tieneBarcode2?.[String(did)]?.empresa_vinculada ?? null;
+        return this.tieneBarcode?.[String(did)]?.empresa_vinculada ?? null;
     }
 
-    // 👉 Método para obtener seller_id
-    static getSellerId(did) {
-        return this.tieneBarcode2?.[String(did)]?.seller_id ?? 0;
+    static getSenderId(did) {
+        return this.tieneBarcode?.[String(did)]?.seller_id ?? 0;
     }
 }
-
-// 3 casos de verificacion si el envio esta ingresado en la logistica interna o externa
-// 1 esta en la externa y no en la interna
-// 2 esta en ambas
-// 3 no esta en ninguna
