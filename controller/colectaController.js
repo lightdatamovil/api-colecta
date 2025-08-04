d5cf794aa0f18652247690c2bd10ffcd7158a18d
Gonn01 <gonzalomiguelrigoni@gmail.com>
2025-08-04 16:32:39 -0300

    refactor: update handleInternalFlex call to accept company object instead of ID

--- controller/colectaController.js
+++ controller/colectaController.js
@@ -111,7 +111,7 @@ export async function colectar(company, dataQr, userId, profile, autoAssign, lat
 
             if (account) {
                 logCyan("Es interno");
-                response = await handleInternalFlex(dbConnection, company.did, userId, profile, dataQr, autoAssign, account, latitude, longitude, senderId);
+                response = await handleInternalFlex(dbConnection, company, userId, profile, dataQr, autoAssign, account, latitude, longitude, senderId);
 
                 /// Si la cuenta no existe, es externo
             }
