cafb64c510b36bf0fb7787f52e3e332c1e8e9105
Gonn01 <gonzalomiguelrigoni@gmail.com>
2025-09-15 11:14:45 -0300

    fix: change HTTP method from GET to POST for '/colecta' route and correct parameter names

--- routes/colecta.js
+++ routes/colecta.js
@@ -3,14 +3,14 @@ import { colectar } from "../controller/colectaController.js";
 import { buildHandlerWrapper } from "../src/funciones/build_handler_wrapper.js";
 
 const colecta = Router();
-colecta.get(
+colecta.post(
   '/colecta',
   buildHandlerWrapper({
     required: [
       "dataQr",
       "autoAssign",
-      "ilat",
-      "ilong",
+      "latitude",
+      "longitude",
     ],
     controller: async ({ db, req, company }) => {
       const result = await colectar(db, req, company);
