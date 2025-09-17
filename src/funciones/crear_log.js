75f11f3b6318dd3658579d1b96941067710a68ae
Gonn01 <gonzalomiguelrigoni@gmail.com>
2025-09-17 09:58:25 -0300

    fix: stringify request body in crearLog function for consistent logging

--- src/funciones/crear_log.js
+++ src/funciones/crear_log.js
@@ -14,7 +14,7 @@ export async function crearLog(req, tiempo, resultado, exito) {
     companyId,
     userId,
     profile,
-    req.body,
+    JSON.stringify(req.body),
     resultado,
     tiempo,
     exito,
