3016293a2b2191b54c4ec37b9e576ed913fcf4e3
denu <nissgonzalezm@gmail.com>
2025-10-15 16:35:29 -0300

    zx

--- controller/test.js
+++ controller/test.js
@@ -1,12 +1,17 @@
 import axios from "axios";
 
 const client = axios.create({
-    baseURL: "http://10.70.0.69:44336",
+    baseURL: "https://10.70.0.69:44336",
+    timeout: 100,
+});
+
+const clientAnterior = axios.create({
+    baseURL: "https://serverestado.lightdata.app/test",
     timeout: 100,
 });
 
 export async function obtenerEstado() {
-    const resp = await client.get("/test");
+    const resp = await clientAnterior.get("/test");
     return { data: resp.data, status: resp.status };
 }
 
